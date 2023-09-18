f6e6dc5388ebcfe80deeb816604cc846ae61ec87
gepasgoy <144433800+gepasgoy@users.noreply.github.com>
2023-09-18 17:48:17 +0300

    randomizer.py

--- randomizer.py
+++ randomizer.py
@@ -7,17 +7,17 @@ print("If you want to complete the randomizer, write stop")
 print("Enter range from: ")
 
 while tr == True:
-    diap1 = input()
-    if diap1.isdigit():
-        diap1 = int(diap1)
+    rng1 = input()
+    if rng1.isdigit():
+        rng1 = int(diap1)
         print("Enter range to: ")
 
-        diap2 = int(input())
+        rng2 = int(input())
 
-        print("result: ", random.randint(diap1,diap2))
+        print("result: ", random.randint(rng1,rng2))
         
         print("Enter range from: ")
-    elif diap1 == "stop":
+    elif rng1 == "stop":
         break
     else:
         print("error!")
